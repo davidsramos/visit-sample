bdb2f2108d05da2ba165b81a0d103d4e9b1a924a
David Ramos <davidsramos@gmail.com>
2020-05-27 23:02:46 +0200

    Begining of repository

---
+++ fivisitf-parent/fivisit-api/src/main/java/ec/ep/info/fivisit/api/FivisitApiApplication.java
@@ -0,0 +1,13 @@
+package ec.ep.info.fivisit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivisitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivisitApiApplication.class, args);
+	}
+
+}
---
+++ fivisitf-parent/fivisit-api/src/test/java/ec/ep/info/fivisit/api/FivisitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.ep.info.fivisit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivisitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
